c414de6bada4ae87365a41027e553e3c2900fcbb
NikhilAnkola <nikhilankola2@gmail.com>
2026-08-04 22:12:55 +0530

    Created a Menu for To-Do List

---
+++ mainProject.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class mainProject{
+    public static void main(String[] args) {
+        boolean isTrue = true;
+
+        while(isTrue){
+            System.out.println("----Menu----");
+            System.out.println("1 : Add Task");
+            System.out.println("2 : View Task");
+            System.out.println("3 : Remove Task");
+            System.out.println("4 : Exit");
+        }
+    }
+}
