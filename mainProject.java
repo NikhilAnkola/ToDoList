d5b29d558ebe1f37269f3430d6afa52ec80c59cd
NikhilAnkola <nikhilankola2@gmail.com>
2026-08-04 22:29:34 +0530

    Added case 1
    Case 1 is to Add a task

--- mainProject.java
+++ mainProject.java
@@ -1,7 +1,10 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class mainProject{
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
+
         boolean isTrue = true;
 
         while(isTrue){
@@ -10,6 +13,19 @@ public class mainProject{
             System.out.println("2 : View Task");
             System.out.println("3 : Remove Task");
             System.out.println("4 : Exit");
+
+            int choice = sc.nextInt();
+
+            switch(choice){
+                case 1: 
+                System.out.print("Enter a task : ");
+                String task = sc.nextLine();
+                tasks.add(task);
+                System.out.println("Task has been added");
+                break;
+            }
         }
+
+        sc.close();
     }
 }
